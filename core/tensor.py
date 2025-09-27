5e09777ee85ff513de3908e202c3831febffc920
omkos333 <omkos333@gmail.com>
2025-09-27 17:13:42 -0400

    small issue fixed in tensor.py

--- core/tensor.py
+++ core/tensor.py
@@ -31,10 +31,6 @@ class Tensor:
         return id(self)
 
     @property
-    def data(self):
-        return Tensor(self.data, requires_grad=False)
-
-    @property
     def shape(self):
         if self.data is None:
             raise AttributeError("cannot access .shape for a Tensor with no data")
